4bc96a6c2d6b392274afd2a3d29e0ced8e111540
Hannes <hannes.ryden@gmail.com>
2017-07-27 17:35:49 +0200

    Cleanup

--- utils/socket-server.js
+++ utils/socket-server.js
@@ -1,11 +1,12 @@
 var config = require.main.require('./config.js');
 
+// Custom socket server with support for bundles, i.e. extensions
 module.exports = {
 	bundles: {},
 	
 	// Start socket server
 	start: function(io) {
-		// Load bundles, i.e. extensions
+		// Load bundles
 		this.loadBundles();
 		
 		// Listener for new connections to the socket server
@@ -15,16 +16,21 @@ module.exports = {
 		}.bind(this));
 	},
 	
-	// Load bundles as defined in config-file 
+	// Load all bundles that are defined in the config-file 
 	loadBundles: function() {
 		for (var i = 0; i < config.bundles.length; i++) {
 			var bundleName = config.bundles[i];
-			this.bundles[bundleName] = require.main.require('./bundles/' + bundleName);
+			this.loadBundle(bundleName);
+		}
+	},
+	
+	// Loads a specific bundle by name
+	loadBundle: function(bundleName) {
+		this.bundles[bundleName] = require.main.require('./bundles/' + bundleName);
 			
-			// If bundle has an init function, then call it.
-			if ("init" in this.bundles[bundleName]) {
-				this.bundles[bundleName].init();
-			}
+		// If bundle has an init function, then call it.
+		if ("init" in this.bundles[bundleName]) {
+			this.bundles[bundleName].init();
 		}
 	},
 	
@@ -37,26 +43,4 @@ module.exports = {
 		socket.removeAllListeners(); // Remove all listeners from previous bundle
 		this.bundles[bundleName].run(socket); // Call run() method on bundle
 	},
-	
-	// Custom method that loads and runs a specific parser
-	/*changeParser: function(parser, socket) {
-		socket.removeAllListeners(); // Remove all previous listeners from previous parser
-		
-		socket.parser = require.main.require('./parsers/' + parser);
-		socket.parser.socket = socket; // Add reference to socket in parser object
-		
-		// Run parser init method
-		socket.parser.init();
-	},*/
-
 }
-
-
-/*
-setInterval(() => {
-	io.emit('new message', {
-	  username: "Server",
-	  message: "Hej hej!"
-	});
-}, 1000);
-*/
