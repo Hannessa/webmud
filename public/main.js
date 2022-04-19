819d77f4687021a3fa9e9dec3cc62200ba14f54e
Simon Kenny <1c3k1ng@tutanota.com>
2022-04-19 22:13:16 +0100

    Fix up public folder, formatting code; clean out main.js of unused JS code from the chat example

--- public/main.js
+++ public/main.js
@@ -1,271 +1,92 @@
-
 $(function() {
-  var FADE_TIME = 200; // 200 ms
-  var TYPING_TIMER_LENGTH = 400; // ms
-  var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-  ];
+    // initial variables
+    const $window = $(window);
+    const $terminalElement = $('.messages');
+    const $inputElement = $('.inputMessage');
+    const socket = io();
 
-  var $messages = $('.messages');
-  var term = new Terminal({
-    convertEol: true,
-    rows: 40
-  });
-  //var termFitAddon = new FitAddon.FitAddon();
-  //term.loadAddon(termFitAddon);
-  term.open($messages[0]);
-  //termFitAddon.fit();
-  
-  //term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-  term.write("\n");
+    let connected = false;
 
-  // Initialize variables
-  var $window = $(window);
-  var $usernameInput = $('.usernameInput'); // Input for username
-  var $inputMessage = $('.inputMessage'); // Input message input box
+    // set up terminal
+    const term = new Terminal({
+        convertEol: true,
+        rows: 40
+    });
+    term.open($terminalElement[0]);
+    term.write("\n");
 
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
+    $inputElement.focus();
 
-  // Prompt for setting a username
-  var username = "Hannes";
-  var isPassword = false;
-  var connected = false;
-  var typing = false;
-  var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
+    // sends a chat message
+    function sendMessage () {
+        let message = $inputElement.val();
+        // Prevent markup from being injected into the message
+        message = cleanInput(message);
+        // if there is a non-empty message and a socket connection
+        if (message && connected) {
+            $inputElement.val('');
+            if ($('.inputMessage').prop("type") == "password") {
+                // Password entered
+                terminalOutput({
+                    msg: '> *******'
+                });
+            } else {
+                // Normal command
+                terminalOutput({
+                    msg: '> ' + message
+                });
+            }
 
-  var socket = io();
-
- $inputMessage.focus();
-  
-  function addParticipantsMessage (data) {
-    var message = '';
-    if (data.numUsers === 1) {
-      message += "there's 1 participant";
-    } else {
-      message += "there are " + data.numUsers + " participants";
+            // Send message to server
+            // tell server to execute 'new message' and send along one parameter
+            socket.emit('input', {"msg": message});
+        }
     }
-    log(message);
-  }
 
-  // Sets the client's username
-  function setUsername () {
-    username = cleanInput($usernameInput.val().trim());
-
-    // If the username is valid
-    if (username) {
-      $loginPage.fadeOut();
-      $chatPage.show();
-      $loginPage.off('click');
-      $currentInput = $inputMessage.focus();
-
-      // Tell the server your username
-      socket.emit('add user', username);
+    // add output to terminal
+    function terminalOutput (data) {
+        term.write(data.msg + "\n")
     }
-  }
 
-  // Sends a chat message
-  function sendMessage () {
-    var message = $inputMessage.val();
-    // Prevent markup from being injected into the message
-    message = cleanInput(message);
-    // if there is a non-empty message and a socket connection
-    if (message && connected) {
-        $inputMessage.val('');
-        if ($('.inputMessage').prop("type") == "password") {
-          // Password entered
-          addChatMessage({
-              //username: username,
-              msg: '> *******'
-              //msg: '<div style="margin-top: 5px;"></div>'
-            });
+    // prevents input from having injected markup
+    function cleanInput (input) {
+        return $('<div/>').text(input).text();
+    }
+
+    // - keyboard events
+
+    $window.keydown(function (event) {
+        // auto-focus the current input when a key is typed
+        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $inputElement.focus();
+        }
+        // ENTER key event
+        if (event.which === 13) {
+            sendMessage();
+        }
+    });
+
+    // - click events
+
+    // Focus input when clicking on the message input's border
+    $inputElement.click(function () {
+        $inputElement.focus();
+    });
+
+    // - socket events
+
+    // whenever the server emits 'output', update the chat body
+    socket.on('output', function (data) {
+        connected = true;
+        data.msg = "\n" + data.msg + "\n";
+
+        // If we expect password in return
+        if (data.password) {
+            $('.inputMessage').prop("type", "password");
         } else {
-          // Normal command
-          addChatMessage({
-            //username: username,
-            msg: '> ' + message
-            //msg: '<div style="margin-top: 5px;"></div>'
-          });
+            $('.inputMessage').prop("type", "text");
         }
 
-        // Send message to server
-        // tell server to execute 'new message' and send along one parameter
-        socket.emit('input', {"msg": message});
-      }
-
-  }
-
-  // Log a message
-  function log (message, options) {
-    term.write('System message: ' + message + "\n\n")
-  }
-
-  // Adds the visual chat message to the message list
-  function addChatMessage (data, options) {
-    term.write(data.msg + "\n")
-  }
-
-  // Adds the visual chat typing message
-  function addChatTyping (data) {
-    data.typing = true;
-    data.message = 'is typing';
-    addChatMessage(data);
-  }
-
-  // Removes the visual chat typing message
-  function removeChatTyping (data) {
-    getTypingMessages(data).fadeOut(function () {
-      $(this).remove();
+        terminalOutput(data);
     });
-  }
-
-  // Prevents input from having injected markup
-  function cleanInput (input) {
-    return $('<div/>').text(input).text();
-  }
-
-  // Updates the typing event
-  function updateTyping () {
-    if (connected) {
-      if (!typing) {
-        typing = true;
-        socket.emit('typing');
-      }
-      lastTypingTime = (new Date()).getTime();
-
-      setTimeout(function () {
-        var typingTimer = (new Date()).getTime();
-        var timeDiff = typingTimer - lastTypingTime;
-        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          socket.emit('stop typing');
-          typing = false;
-        }
-      }, TYPING_TIMER_LENGTH);
-    }
-  }
-
-  // Gets the 'X is typing' messages of a user
-  function getTypingMessages (data) {
-    return $('.typing.message').filter(function (i) {
-      return $(this).data('username') === data.username;
-    });
-  }
-
-  // Gets the color of a username through our hash function
-  function getUsernameColor (username) {
-    // Compute hash code
-    var hash = 7;
-    for (var i = 0; i < username.length; i++) {
-       hash = username.charCodeAt(i) + (hash << 5) - hash;
-    }
-    // Calculate color
-    var index = Math.abs(hash % COLORS.length);
-    return COLORS[index];
-  }
-
-  // Keyboard events
-
-  $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
-    // When the client hits ENTER on their keyboard
-    if (event.which === 13) {
-      if (username) {
-        sendMessage();
-        //socket.emit('stop typing');
-        typing = false;
-      } else {
-        setUsername();
-      }
-    }
-  });
-
-  $inputMessage.on('input', function() {
-    //updateTyping();
-  });
-
-  // Click events
-
-  // Focus input when clicking anywhere on login page
-  $loginPage.click(function () {
-    $currentInput.focus();
-  });
-
-  // Focus input when clicking on the message input's border
-  $inputMessage.click(function () {
-    $inputMessage.focus();
-  });
-
-  // Socket events
-
-  // Whenever the server emits 'new message', update the chat body
-  socket.on('output', function (data) {
-    connected = true;
-	  data.msg = data.msg + "\n";
-
-    // If we expect password in return
-    if (data.password) {
-      $('.inputMessage').prop("type", "password");
-    } else {
-      $('.inputMessage').prop("type", "text");
-    }
-
-    addChatMessage(data);
-  });
-  
-  // Whenever the server emits 'login', log the login message
-  socket.on('login', function (data) {
-    connected = true;
-    // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
-    log(message, {
-      prepend: true
-    });
-    addParticipantsMessage(data);
-  });
-
-
-  // Whenever the server emits 'user joined', log it in the chat body
-  socket.on('user joined', function (data) {
-    log(data.username + ' joined');
-    addParticipantsMessage(data);
-  });
-
-  // Whenever the server emits 'user left', log it in the chat body
-  socket.on('user left', function (data) {
-    log(data.username + ' left');
-    addParticipantsMessage(data);
-    removeChatTyping(data);
-  });
-
-  // Whenever the server emits 'typing', show the typing message
-  socket.on('typing', function (data) {
-    addChatTyping(data);
-  });
-
-  // Whenever the server emits 'stop typing', kill the typing message
-  socket.on('stop typing', function (data) {
-    removeChatTyping(data);
-  });
-
-  socket.on('disconnect', function () {
-    log('You have been disconnected.');
-  });
-
-  socket.on('reconnect', function () {
-    log('You have been reconnected.');
-    if (username) {
-      socket.emit('add user', username);
-    }
-  });
-
-  socket.on('reconnect_error', function () {
-    log('Attempt to reconnect has failed.');
-  });
-
 });
